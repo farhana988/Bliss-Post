86154a7de2395e1aab0dcffb31ab362509f781da
farhana988 <farhana744255@gmail.com>
2025-01-23 23:48:52 +0600

    test

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -5,31 +5,23 @@ import React from "react";
 async function page() {
   const { getUser } = getKindeServerSession();
 
-  try {
-    const user = await getUser();
-    
-    if (!user) {
-      console.log("No user session found. Redirecting to login...");
-      redirect("/api/auth/login");
-    }
-
-    return (
-      <div className="min-h-screen pt-16">
-        <h1 className="text-3xl font-bold text-center mb-6 text-gray-900">
-          Profile Page
-        </h1>
-        <div className="max-w-4xl mx-auto p-6 bg-white rounded-lg shadow-lg">
-          <h1 className="text-3xl font-bold mb-6 text-gray-900 text-center">
-            Welcome to your profile
-          </h1>
-      
-        </div>
-      </div>
-    );
-  } catch (error) {
-    console.error("Error fetching user session:", error);
+  const user = await getUser();
+  if (!user) {
     redirect("/api/auth/login");
   }
+
+  return (
+    <div className="min-h-screen pt-16">
+      <h1 className="text-3xl font-bold text-center mb-6 text-gray-900">
+        Profile Page
+      </h1>
+      <div className="max-w-4xl mx-auto p-6 bg-white rounded-lg shadow-lg">
+        <h1 className="text-3xl font-bold mb-6 text-gray-900 text-center">
+          Welcome to your profile!
+        </h1>
+      </div>
+    </div>
+  );
 }
 
 export default page;
