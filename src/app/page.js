65eebc61d6294017c4e6cb1bce961171bd5276dc
farhana988 <farhana744255@gmail.com>
2025-01-23 10:59:10 +0600

    fetched the data

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,21 @@
 import Image from "next/image";
+import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const posts = await res.json();
   return (
     <div className="text-3xl">
      hello
+     <ul>
+        {posts.map((post) => (
+          <li key={post.id} className="mb-2">
+           
+              <a className="">{post.title}</a>
+            
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
