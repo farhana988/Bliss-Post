4f37a44588c791baf65753994f47130ddf55fedc
farhana988 <farhana744255@gmail.com>
2025-01-24 00:31:32 +0600

    test

--- src/app/api/auth/[kindeAuth]/route.js
+++ src/app/api/auth/[kindeAuth]/route.js
@@ -1,2 +1,12 @@
 import {handleAuth} from "@kinde-oss/kinde-auth-nextjs/server";
 export const GET = handleAuth();
+export const OPTIONS = (req) => {
+    return new Response(null, {
+      status: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+      },
+    });
+  };
--- src/app/api/auth/logout.js
+++ src/app/api/auth/logout.js
@@ -1,38 +1,38 @@
-import Cors from 'cors';
+// import Cors from 'cors';
 
 
-const cors = Cors({
-  origin: 'https://bliss-post.vercel.app', 
-  methods: ['GET', 'POST', 'OPTIONS'],
-});
+// const cors = Cors({
+//   origin: 'https://bliss-post.vercel.app', 
+//   methods: ['GET', 'POST', 'OPTIONS'],
+// });
 
 
-function runMiddleware(req, res, fn) {
-  return new Promise((resolve, reject) => {
-    fn(req, res, (result) => {
-      if (result instanceof Error) {
-        return reject(result);
-      }
-      return resolve(result);
-    });
-  });
-}
+// function runMiddleware(req, res, fn) {
+//   return new Promise((resolve, reject) => {
+//     fn(req, res, (result) => {
+//       if (result instanceof Error) {
+//         return reject(result);
+//       }
+//       return resolve(result);
+//     });
+//   });
+// }
 
-export default async function handler(req, res) {
+// export default async function handler(req, res) {
 
-  await runMiddleware(req, res, cors);
+//   await runMiddleware(req, res, cors);
 
-  if (req.method === 'OPTIONS') {
+//   if (req.method === 'OPTIONS') {
 
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.status(200).end();
-    return;
-  }
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//     res.status(200).end();
+//     return;
+//   }
 
 
-  if (req.method === 'GET') {
-    res.status(200).json({ message: 'Logout successful' });
-  } else {
-    res.status(405).json({ error: 'Method not allowed' });
-  }
-}
+//   if (req.method === 'GET') {
+//     res.status(200).json({ message: 'Logout successful' });
+//   } else {
+//     res.status(405).json({ error: 'Method not allowed' });
+//   }
+// }
