e38f6a1f72448863f528530d53a0828e1ea5dfa6
farhana988 <farhana744255@gmail.com>
2025-01-23 15:19:48 +0600

    displayed all products

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import PostCard from "@/components/PostCard";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -5,17 +6,20 @@ export default async function Home() {
   const res = await fetch('https://jsonplaceholder.typicode.com/posts');
   const posts = await res.json();
   return (
-    <div className="text-3xl">
-     hello
-     <ul>
-        {posts.map((post) => (
-          <li key={post.id} className="mb-2">
-           
-              <a className="">{post.title}</a>
-            
-          </li>
-        ))}
-      </ul>
+    <div className="text-3xl p-4">
+    <h1 className="text-4xl font-bold mb-6">Blog Posts</h1>
+    <p className="text-lg mb-4">Total Posts: {posts.length}</p>
+
+    {/* Responsive grid for posts */}
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+      {posts.map((post) => (
+       <PostCard
+       key={post.id}
+       post={post}
+       
+       ></PostCard>
+      ))}
     </div>
+  </div>
   );
 }
---
+++ src/components/PostCard.jsx
@@ -0,0 +1,22 @@
+"use client"
+import Link from 'next/link'
+import React from 'react'
+
+function PostCard({post}) {
+  return (
+    <div>
+         <div  className="bg-white p-4 rounded-lg shadow-md flex flex-col 
+        h-40 lg:h-52">
+          <h2 className="text-base lg:text-xl font-semibold mb-2">{post.title}</h2>
+          <p className="text-gray-700 text-xs lg:text-sm flex-1">{post.body}</p>
+          <Link 
+          className="text-xs text-blue-600"
+          href={`/posts/${post.id}`}>
+           Read more
+          </Link>
+        </div>
+    </div>
+  )
+}
+
+export default PostCard
